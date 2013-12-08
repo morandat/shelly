a12b36dfa61a6be06b534e1bbadb11269485a094
Floreal Morandat florealm@gmail.com <florealm@gmail.com>
2013-12-08 22:11:41 +0100

    Fixed two file names

--- src/main/java/fr/labri/shelly/impl/Doctor.java
+++ src/main/java/fr/labri/shelly/impl/Doctor.java
@@ -6,7 +6,11 @@ import javax.annotation.*;
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.util.ElementKindVisitor7;
+
 
 @SupportedSourceVersion(SourceVersion.RELEASE_7)
 @SupportedAnnotationTypes(value= {"fr.labri.shelly.annotations.*"})
@@ -15,10 +19,34 @@ public class Doctor extends AbstractProcessor {
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
 		for (Element elt : roundEnv.getRootElements()) {
-			System.out.println(elt);
-			// FIXME need to build a full model with this
+			elt.accept(new NameVisitor(), null);
 		}
-		System.out.println(roundEnv);
 		return true;
+	}
+	class NameVisitor  extends ElementKindVisitor7<Void, Void> {
+		
+		@Override
+		protected Void defaultAction(Element e, Void p) {
+			return super.defaultAction(e, p);
+		}
+
+		@Override
+		public Void visitTypeAsClass(TypeElement e, Void p) {
+			System.out.println(e);
+			return super.visitTypeAsClass(e, p);
+		}
+
+		@Override
+		public Void visitVariableAsField(VariableElement e, Void p) {
+			System.out.println(e);
+			return super.visitVariableAsField(e, p);
+		}
+
+		@Override
+		public Void visitExecutableAsMethod(ExecutableElement e, Void p) {
+			System.out.println(e);
+			return super.visitExecutableAsMethod(e, p);
+		}
+		
 	}
 }
